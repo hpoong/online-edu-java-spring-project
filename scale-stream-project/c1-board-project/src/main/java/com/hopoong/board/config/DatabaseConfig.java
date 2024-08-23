47335da80befcaeff505d509f553e606e0320e51
ho_poong <hopoong23@gmail.com>
2024-08-23 18:36:50 +0900

    redis 설정 진행중

---
+++ scale-stream-project/c1-board-project/src/main/java/com/hopoong/board/config/DatabaseConfig.java
@@ -0,0 +1,33 @@
+package com.hopoong.board.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@ComponentScan( basePackages = "com.hopoong.board" )
+@MapperScan( basePackages = "com.hopoong.board" )
+@EnableTransactionManagement
+public class DatabaseConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sessionFactory.setMapperLocations(resolver.getResources("classpath:mapper/**/*.xml"));
+        sessionFactory.setConfigLocation(resolver.getResource("classpath:mybatis/mybatis-config.xml"));
+        return sessionFactory.getObject();
+    }
+
+}
