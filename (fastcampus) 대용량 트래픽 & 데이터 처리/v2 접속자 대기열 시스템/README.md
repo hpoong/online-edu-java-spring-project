c5d962830b778ed6342c0a23887dc2d08332872f
ho_poong <hopoong23@gmail.com>
2024-08-26 23:13:30 +0900

    connection_queue_management init commit

---
+++ (fastcampus) 대용량 트래픽 & 데이터 처리/v2 접속자 대기열 시스템/README.md
@@ -0,0 +1,15 @@
+# 프로젝트
+접속자 대기열 시스템 프로젝트
+
+---
+
+# 설치 목록
+
+---
+- java 17
+- docker 
+- redis image
+
+```bash
+docker pull redis:6.2
+```
---
+++ (fastcampus) 대용량 트래픽 & 데이터 처리/v2 접속자 대기열 시스템/src/test/java/com/hopoong/connection_queue_management/ConnectionQueueManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hopoong.connection_queue_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionQueueManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
