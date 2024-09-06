dcde93057a876cd279f9b28ad5d6cd10b60994c0
ho_poong <hopoong23@gmail.com>
2024-09-06 19:15:24 +0900

    #fix 멀티 모듈 생성 yml 설정 변경

---
+++ (fastcampus) 대용량 트래픽 & 데이터 처리/v3 선착순 쿠폰 시스템/coupon-core/src/main/java/com/hopoong/couponcore/CouponCoreConfiguration.java
@@ -0,0 +1,11 @@
+package com.hopoong.couponcore;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@ComponentScan
+@Configuration
+public class CouponCoreConfiguration {
+}
