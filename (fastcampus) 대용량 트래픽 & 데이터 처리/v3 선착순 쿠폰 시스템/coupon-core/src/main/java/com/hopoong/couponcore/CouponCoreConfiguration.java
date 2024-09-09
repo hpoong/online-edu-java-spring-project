4f645893fe63e3884ac64bd19d79976b3ec935b9
ho_poong <hopoong23@gmail.com>
2024-09-09 19:44:33 +0900

    엔티티 추가

--- (fastcampus) 대용량 트래픽 & 데이터 처리/v3 선착순 쿠폰 시스템/coupon-core/src/main/java/com/hopoong/couponcore/CouponCoreConfiguration.java
+++ (fastcampus) 대용량 트래픽 & 데이터 처리/v3 선착순 쿠폰 시스템/coupon-core/src/main/java/com/hopoong/couponcore/CouponCoreConfiguration.java
@@ -3,9 +3,13 @@ package com.hopoong.couponcore;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@EnableJpaAuditing // CreatedDate LastModifiedDate
 @ComponentScan
 @Configuration
+@PropertySource("file:./pwd.ini")
 public class CouponCoreConfiguration {
 }
---
+++ (fastcampus) 대용량 트래픽 & 데이터 처리/v3 선착순 쿠폰 시스템/coupon-core/src/main/java/com/hopoong/couponcore/model/CouponType.java
@@ -0,0 +1,6 @@
+package com.hopoong.couponcore.model;
+
+public enum CouponType {
+
+    FIRST_COME_FIRST_SERVED // 선택순 쿠폰
+}
