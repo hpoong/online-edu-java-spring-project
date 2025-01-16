dad153887bc897cddbf815a696390853600257ea
ho_poong <hopoong23@gmail.com>
2025-01-16 18:51:35 +0900

    featureflag dependencies add . docker images add .

--- (fastcampus) 대용량 트래픽 & 데이터 처리/v7 피처플레그/featureflag-adapter/src/main/java/com/hopoong/featureflag_adapter/FeatureflagAdapterConfig.java
+++ (fastcampus) 대용량 트래픽 & 데이터 처리/v7 피처플레그/featureflag-adapter/src/main/java/com/hopoong/featureflag_adapter/FeatureflagAdapterConfig.java
@@ -1,11 +1,45 @@
 package com.hopoong.featureflag_adapter;
 
 
+import dev.openfeature.contrib.providers.flagd.Config;
+import dev.openfeature.contrib.providers.flagd.FlagdOptions;
+import dev.openfeature.contrib.providers.flagd.FlagdProvider;
+import dev.openfeature.sdk.Client;
+import dev.openfeature.sdk.OpenFeatureAPI;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @ComponentScan
 @EnableAutoConfiguration
+@RequiredArgsConstructor
 public class FeatureflagAdapterConfig {
+
+    private final Environment environment;
+
+    @Bean
+    public Client featureflagAdapter() {
+        String flagdHost = environment.getProperty("FEATUREFLAG_ENGINE_HOST") == null ? "localhost" : environment.getProperty("FEATUREFLAG_ENGINE_HOST");
+        String flagdPort = environment.getProperty("FEATUREFLAG_ENGINE_PORT") == null ? "8013" : environment.getProperty("FEATUREFLAG_ENGINE_PORT");
+
+        FlagdProvider flagd = new FlagdProvider(
+                FlagdOptions.builder()
+                        .resolverType(Config.Evaluator.RPC)
+                        .host(flagdHost)
+                        .port(Integer.parseInt(flagdPort))
+                        .tls(false)
+                        .maxCacheSize(100)
+                        .retryBackoffMs(100)
+                        .deadline(3000)
+                        .cacheType("lru")
+                        .build()
+        );
+
+        OpenFeatureAPI api = OpenFeatureAPI.getInstance();
+        api.setProvider(flagd);
+        return api.getClient();
+    }
 
 }
--- (fastcampus) 대용량 트래픽 & 데이터 처리/v7 피처플레그/featureflag-adapter/src/main/java/com/hopoong/featureflag_adapter/openfeatureflag/FlagdAdapter.java
+++ (fastcampus) 대용량 트래픽 & 데이터 처리/v7 피처플레그/featureflag-adapter/src/main/java/com/hopoong/featureflag_adapter/openfeatureflag/FlagdAdapter.java
@@ -1,18 +1,29 @@
 package com.hopoong.featureflag_adapter.openfeatureflag;
 
 import com.hopoong.featureflag_usecase.port.GetStringValuePort;
+import dev.openfeature.sdk.Client;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class FlagdAdapter implements GetStringValuePort {
+
+    private final Client flagdClient;
 
 
     // flagd SDK client instance
     @Override
     public String getStringValue(GetStringValuePortRequest request) {
-        // get feature from flagd engine
-        return "defulatValue";
+        String result = flagdClient.getStringValue(request.getKey(), "null");
+
+        if(result.equals("null")) {
+            throw new RuntimeException("Flagd returned null for key: " + request.getKey());
+        }
+
+        return result;
     }
 }
