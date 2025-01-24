61ea4ca1c0fd999db8c1b07bbaf11cc7e2be24b0
ho_poong <hopoong23@gmail.com>
2025-01-24 23:49:34 +0900

    docker-compose.yml rabbitmq add

--- (fastcampus) 대용량 트래픽 & 데이터 처리/v8 마이그레이션/dsp-migration-app/src/main/java/com/hopoong/dspmigration/app/legacy/domain/LegacyKeyword.java
+++ (fastcampus) 대용량 트래픽 & 데이터 처리/v8 마이그레이션/dsp-migration-app/src/main/java/com/hopoong/dspmigration/app/legacy/domain/LegacyKeyword.java
@@ -1,18 +1,21 @@
 package com.hopoong.dspmigration.app.legacy.domain;
 
+import com.hopoong.dspmigration.app.legacy.api.keyword.event.LegacyKeywordCreatedEvent;
+import com.hopoong.dspmigration.app.legacy.api.keyword.event.LegacyKeywordDeletedEvent;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.AbstractAggregateRoot;
 
 import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @Getter
-public class LegacyKeyword {
+public class LegacyKeyword extends AbstractAggregateRoot<LegacyKeyword> {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,19 +26,21 @@ public class LegacyKeyword {
     private LocalDateTime createdAt;
     private LocalDateTime deletedAt;
 
-    private LegacyKeyword(String text, Long adGroupId, Long userId, LocalDateTime createdAt) {
+    private LegacyKeyword(String text, Long adGroupId, Long userId, LocalDateTime createdAt, LocalDateTime deletedAt) {
         this.text = text;
         this.adGroupId = adGroupId;
         this.userId = userId;
         this.createdAt = createdAt;
-        this.deletedAt = null;
+        this.deletedAt = deletedAt;
+        registerEvent(new LegacyKeywordCreatedEvent(this));
     }
 
     public static LegacyKeyword of(String text, Long adGroupId, Long userId) {
-        return new LegacyKeyword(text, adGroupId, userId, LocalDateTime.now());
+        return new LegacyKeyword(text, adGroupId, userId, LocalDateTime.now(), null);
     }
 
     public void delete() {
         deletedAt = LocalDateTime.now();
+        registerEvent(new LegacyKeywordDeletedEvent(this));
     }
 }
