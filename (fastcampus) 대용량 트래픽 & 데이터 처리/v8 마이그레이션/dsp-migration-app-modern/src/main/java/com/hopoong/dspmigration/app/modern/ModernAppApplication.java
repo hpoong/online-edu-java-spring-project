ab85efab056e44b94b88eddb7f2c4c2970ed89b2
ho_poong <hopoong23@gmail.com>
2025-01-25 16:26:50 +0900

    - dsp-migration-app-legacy module name 변경 - dsp-migration-app-modern module 추가

---
+++ (fastcampus) 대용량 트래픽 & 데이터 처리/v8 마이그레이션/dsp-migration-app-modern/src/main/java/com/hopoong/dspmigration/app/modern/ModernAppApplication.java
@@ -0,0 +1,13 @@
+package com.hopoong.dspmigration.app.modern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModernAppApplication.class, args);
+    }
+
+}
